bf1003954e48793c7b1273b948ce25a2488ffe41
Sergey <sergey@heyphets.com>
2016-09-10 16:58:42 -0400

    Module 1 solution final

---
+++ module1-solution/app.js
@@ -0,0 +1,26 @@
+(function () {
+  'use strict'
+
+  angular.module('LunchCheck',[])
+  .controller('LunchCheckController',LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController($scope){
+    $scope.inputLunch='';
+    $scope.hasResult=false;
+    $scope.resultMessage='Please enter data first';
+    $scope.checkTooMuch = function () {
+      if ($scope.inputLunch.trim().length==0) {
+        $scope.resultMessage='Please enter data first';
+        $scope.hasResult=false;
+        return;
+        }
+      $scope.hasResult=true;
+      var menuCount=$scope.inputLunch.split(',').length;
+      $scope.resultMessage=(menuCount)<=3?'Enjoy!':'Too much!';
+
+    };
+  };
+
+})();
